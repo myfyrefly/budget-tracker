e0b455538bb7e81fd6681a80ae3adc1aea02f7f8
sofiaM <smejia03@gmail.com>
2020-05-30 16:52:27 -0500

    mongodb_uri corrected

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-
+const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -15,9 +15,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || process.env.REMOTE;
+var MONGODB_URI = process.env.MONGODB_URI || process.env.REMOTE || "mongodb://localhost/budget";
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect( MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true
